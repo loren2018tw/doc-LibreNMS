c2896d4664a0edd96a3c21ae1a71b1e6b0da3761
Loren Li <loren.tw@gmail.com>
2023-12-14 21:06:19 +0800

    12142106

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,17 @@
 import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
-  lang: "zh-CTW",
+  lang: "zh-TW",
   title: "LibreNMS 安裝與使用",
   description: "LibreNMS 安裝與使用",
   base: "/doc-LibreNMS/",
 });
+
+// // .vuepress/config.js
+// module.exports = {
+//   lang: "zh-TW",
+//   title: "LibreNMS 安裝與使用",
+//   description: "LibreNMS 安裝與使用",
+//   // base: "/doc-LibreNMS/",
+//   theme: "gitbook",
+// };
